2281a6ce820d06bba992e298da574d7b65bf2998
westsider <whansen1@mac.com>
2021-08-04 06:33:48 -0500

    initial commit

--- ARKit Test/ViewController.swift
+++ ARKit Test/ViewController.swift
@@ -7,18 +7,42 @@
 
 import UIKit
 import RealityKit
+import Firebase
 
 class ViewController: UIViewController {
     
     @IBOutlet var arView: ARView!
     
+    let email = "whansen1@mac.com"
+    let password = "123456"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        signIn()
         // Load the "Box" scene from the "Experience" Reality File
         let boxAnchor = try! Experience.loadBox()
         
         // Add the box anchor to the scene
         arView.scene.anchors.append(boxAnchor)
+        
+        
+    }
+    
+    func createNewUser() {
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                print("we had an error creating a user \(error)")
+            }
+            print("new user set")
+        }
+    }
+    
+    func signIn() {
+        Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+          guard let strongSelf = self else { return }
+            print(authResult)
+            print("Strong self signed in: \(strongSelf.email)")
+        }
     }
 }
