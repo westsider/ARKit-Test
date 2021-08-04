3e43db460f7c76c8b7f28249a4ea1153cabce41e
westsider <whansen1@mac.com>
2021-08-04 06:54:34 -0500

    arkit setup

--- ARKit Test/ViewController.swift
+++ ARKit Test/ViewController.swift
@@ -8,25 +8,57 @@
 import UIKit
 import RealityKit
 import Firebase
+import ARKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSessionDelegate, ARSCNViewDelegate {
     
-    @IBOutlet var arView: ARView!
+  
+    @IBOutlet var sceneView: ARSCNView!
     
     let email = "whansen1@mac.com"
     let password = "123456"
-    
+  
     override func viewDidLoad() {
         super.viewDidLoad()
         
         signIn()
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        sceneView.delegate = self
+        sceneView.session.delegate = self
+        sceneView.automaticallyUpdatesLighting = true
         
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         
+        // AR experiences typically involve moving the device without
+        // touch input for some time, so prevent auto screen dimming.
+        UIApplication.shared.isIdleTimerDisabled = true
         
+        // "Reset" to run the AR session for the first time.
+        resetTracking()
+    }
+    
+    /// - Tag: ARFaceTrackingSetup
+    func resetTracking() {
+        guard ARFaceTrackingConfiguration.isSupported else { return }
+        let configuration = ARFaceTrackingConfiguration()
+        if #available(iOS 13.0, *) {
+            configuration.maximumNumberOfTrackedFaces = ARFaceTrackingConfiguration.supportedNumberOfTrackedFaces
+        }
+        configuration.isLightEstimationEnabled = true
+        sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+        
+    }
+    
+    // Auto-hide the home indicator to maximize immersion in AR experiences.
+    override var prefersHomeIndicatorAutoHidden: Bool {
+        return true
+    }
+    
+    // Hide the status bar to maximize immersion in AR experiences.
+    override var prefersStatusBarHidden: Bool {
+        return true
     }
     
     func createNewUser() {
